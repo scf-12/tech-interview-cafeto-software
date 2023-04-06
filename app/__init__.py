0eac888c358063630ccb6670d7fb2184c8fa7222
scf-12 <cortesf.santiago@gmail.com>
2023-04-06 10:41:52 -0500

    Initial flask configuration

---
+++ app/__init__.py
@@ -0,0 +1,7 @@
+"""
+Flask application configuration file, in charge of initializing
+and setting the required information for the app to execute.
+"""
+from flask import Flask
+
+app = Flask(__name__)
