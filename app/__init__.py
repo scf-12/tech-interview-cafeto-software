60cff829c3891a3432160e5a2f890c868c5febf6
scf-12 <cortesf.santiago@gmail.com>
2023-04-06 10:48:44 -0500

    Added circular dependencies

--- app/__init__.py
+++ app/__init__.py
@@ -5,3 +5,7 @@ and setting the required information for the app to execute.
 from flask import Flask
 
 app = Flask(__name__)
+
+from app import views
+from app import models
+from app import handlers
